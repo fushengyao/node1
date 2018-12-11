d004f253a164d433d31ba273a55bd2fc0dea5027
xuzhenwu <1174592047@qq.com>
2018-12-11 14:46:12 +0800

    a
    连接池优化

--- database/connection.js
+++ database/connection.js
@@ -1,27 +1,77 @@
 var mysql = require("mysql");
+var async = require("async");
 //mysql连接池
 var mysqlConnectionPool = mysql.createPool({
     host: '120.27.211.41',
     user: 'root',
     password: "",
-    database: 'mysql',
+    database: 'myWeb',
     port: "3306",
     multipleStatements: true  //开启多sql
 })
 
-var query = function(sql, cb) {
-    mysqlConnectionPool.getConnection(function (err,connection) {
-        if (err) {
-            cb(err, null)
-        } else {
-            connection.query(sql, function (qerr, vals) {
-                //释放连接
-                connection.release();
-                //事件驱动回调
-                cb(qerr, vals)
+var query = function(sql,params, callback) {
+    async.waterfall([
+        function(cb){
+            mysqlConnectionPool.getConnection((err, connection)=>{
+                if (err) {
+                    callback(err, null);
+                }
+                cb(err, connection)
+            })
+        },
+        function(connection, cb){
+            connection.beginTransaction((err) => {
+                if (err) {
+                    callback(err, null)
+                }
+                cb(err, connection)
+            })
+        },
+        function(connection, cb){
+            connection.query(sql, params, (err, results) => {
+                if (err) {
+                    connection.rollback(()=> {
+                        callback(err, null);
+                        console.log("事务执行失败, error" + err);
+                        throw err;
+                    })
+                } else {
+                    callback(null, results)
+                    return;
+                }
+                cb(err, null)
             })
         }
-    })
+    ], function(err, results){
+        console.log(err);
+        console.log(results);
+    });
+    // mysqlConnectionPool.getConnection((err, connection) => {
+    //     if (err) {
+    //         callback(err, null);
+    //         return;
+    //     }
+    //     //开启事务
+    //     connection.beginTransaction((err)=> {
+    //         if (err) {
+    //             callback(err, null);
+    //             return;
+    //         }
+    //         //开始执行sql
+    //         connection.query(sql, params, (err, results) => {
+    //             if (err) {
+    //                 connection.rollback(()=> {
+    //                     console.log("事务执行失败, error" + err);
+    //                     throw err;
+    //                 })
+    //             } else {
+    //                 callback(null, results)
+    //                 return;
+    //             }
+    //         })
+    //     })
+    // })
 }
 
 module.exports = query
--- routes/users.js
+++ routes/users.js
@@ -7,24 +7,22 @@ router.use(function (req, res, next) {
 })
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    console.log(111, 222);
-    // query("select u.host, u.user, u.password from user u", (err, results,)=>{
-    //     res.send(results);
-    // })
 
-   // res.status(500).send({error:'Something blew up!'});
-res.send({a: 111})
 });
 
-router.post('/', function(req, res, next) {
-    console.log(req.body);
-    console.log(req.params);
-    // query("select u.host, u.user, u.password from user u", (err, results,)=>{
-    //     res.send(results);
-    // })
-
-    // res.status(500).send({error:'Something blew up!'});
-    res.send({a: 111})
+router.post('/login', function(req, res, next) {
+    var user = req.body.user;
+    query("select * from users where username = ? and password = ?",[user.username, user.password], (err, results)=>{
+        var flag = true;
+        var msg = "查询成功"
+        if (err) {
+            flag = false;
+            msg = "查询失败，请稍后重试";
+            console.log("查询失败，请稍后重试");
+            return
+        }
+        res.send({flag: flag, results: results, msg: msg});
+    })
 });
 
 module.exports = router;
